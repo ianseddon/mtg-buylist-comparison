dd0ae7854dac0653bf3c0082da7f8f1fb49f3928
Ian Seddon <ianseddon55@gmail.com>
2018-01-29 11:02:54 -0700

    Mock having a separate card reference table.

--- app/CardListItem.php
+++ app/CardListItem.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class CardListItem extends Model
 {
+    protected $appends = ['card'];
     protected $fillable = ['quantity', 'set', 'name', 'foil', 'language', 'condition'];
+
+    public function getCardAttribute()
+    {
+        return [
+            'name' => $this->name,
+            'set' => $this->set,
+        ];
+    }
 
     public function list()
     {
