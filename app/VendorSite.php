549ed870f3d65078a05d0c7eb6c83417111fdcc6
Ian Seddon <ianseddon55@gmail.com>
2018-01-30 21:33:29 -0700

    Create relationship from VendorSite to that site's BuyOrders.

--- app/VendorSite.php
+++ app/VendorSite.php
@@ -9,6 +9,16 @@ class VendorSite extends Model
     protected $fillable = ['name', 'scraper_class'];
 
     /**
+     * Relationship to this site's posted buy orders.
+     *
+     * @return void
+     */
+    public function buyOrders()
+    {
+        return $this->hasMany(BuyOrder::class);
+    }
+
+    /**
      * Get an instance of the scraper for this site.
      *
      * @return App\Scraping\Scraper
